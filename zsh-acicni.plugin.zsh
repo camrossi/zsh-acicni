e30a28e9ca725aef805b1eb5401139507807132d
Camillo Rossi <camrossi@cisco.com>
2019-05-28 10:02:42 +1000

    restart accc

--- zsh-acicni.plugin.zsh
+++ zsh-acicni.plugin.zsh
@@ -1,3 +1,3 @@
 # get aci-cc logs 
 alias acc-logs='kubectl -n kube-system logs `kubectl -n kube-system get pod -l name=aci-containers-controller -o name`'
-
+alias acc-restart='kubectl -n kube-system delete `kubectl -n kube-system get pod -l name=aci-containers-controller -o name`'
